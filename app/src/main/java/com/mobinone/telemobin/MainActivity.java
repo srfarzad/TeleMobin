8f458c832e2a7ff8d422215c888f109f96fc5b19
Android <sr.farzad@gmail.com>
2018-07-03 12:01:46 +0430

    session 4 option menu material design resources

--- app/src/main/java/com/mobinone/telemobin/MainActivity.java
+++ app/src/main/java/com/mobinone/telemobin/MainActivity.java
@@ -2,9 +2,13 @@ package com.mobinone.telemobin;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,11 +16,19 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+            toolbar=findViewById(R.id.toolbar);
+
+            setSupportActionBar(toolbar);
+
+            getSupportActionBar().setTitle("");
 
         ImageView img=findViewById(R.id.img_slider);
 
@@ -92,5 +104,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.e("LifeCycle","onResume");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.home_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.item_about:
+
+                Intent intent=new Intent(getApplicationContext(),
+                        AboutActivity.class);
+
+                startActivity(intent);
+
+
+                break;
+
+            case R.id.item_contact:
+
+
+                Intent call_intent=new Intent(Intent.ACTION_VIEW, Uri.parse("tel:0912365"));
+                //Intent call_intent=new Intent(Intent.ACTION_VIEW, Uri.parse("sms:0912365"));
+                startActivity(call_intent);
+
+
+                break;
+
+            case R.id.item_exit:
+
+                finish();
+
+                break;
+            case R.id.item_website:
+
+                //webview
+                Intent website_intent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.p30droid.com"));
+                startActivity(website_intent);
+                break;
+
+
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
